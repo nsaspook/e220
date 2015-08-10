164d6545cd439bc62b5d44c6090ef623ec5d15ec
Fred Brooks <fred@sma2.rain.com>
2015-08-09 18:17:37 -0700

    update

--- ll_defs.h
+++ ll_defs.h
@@ -12,10 +12,10 @@
 extern "C" {
 #endif
 
-#define	CHECKMARK_CRC	0x5757			// 16 bit crc checkmark
+#define	CHECKMARK_CRC		0x5757			// 16 bit crc checkmark
 #define	MAGIC			0x0300  		// data version checkmark
-#define LOW_VECTOR      0x18            // ISR low address
-#define HIGH_VECTOR     0x8             // ISR high address
+#define LOW_VECTOR		0x18            // ISR low address
+#define HIGH_VECTOR		0x8             // ISR high address
 
 #define BAUD_FAST		41 // 375000 BPS is 41, high speed testing is 2
 #define BAUD_SLOW		0
@@ -25,23 +25,23 @@ extern "C" {
 
 #define	PDELAY			10000	// 36hz refresh for HID
 #define	TIMEROFFSET		26474	// timer0 16bit counter value for 1 second to overflow
-#define SLAVE_ACTIVE	10		// Activity counter level
+#define SLAVE_ACTIVE		10	// Activity counter level
 
 	/* DIO defines */
-#define LOW				(unsigned char)0        // digital output state levels, sink
+#define LOW			(unsigned char)0        // digital output state levels, sink
 #define	HIGH			(unsigned char)1        // digital output state levels, source
-#define IN				HIGH
-#define OUT				LOW
-#define	ON				LOW       		//
-#define OFF				HIGH			//
-#define	S_ON            LOW       		// low select/on for chip/led
-#define S_OFF           HIGH			// high deselect/off chip/led
-#define	R_ON            HIGH       		// control relay states, relay is on when output gate is high
-#define R_OFF           LOW				// control relay states
-#define R_ALL_OFF       0x00
+#define IN			HIGH
+#define OUT			LOW
+#define	ON			LOW       		//
+#define OFF			HIGH			//
+#define	S_ON			LOW       		// low select/on for chip/led
+#define S_OFF			HIGH			// high deselect/off chip/led
+#define	R_ON			HIGH       		// control relay states, relay is on when output gate is high
+#define R_OFF			LOW				// control relay states
+#define R_ALL_OFF		0x00
 #define R_ALL_ON		0xff
-#define NO				LOW
-#define YES				HIGH
+#define NO			LOW
+#define YES			HIGH
 
 #define DLED0			LATDbits.LATD0
 #define DLED1			LATDbits.LATD1
@@ -49,27 +49,27 @@ extern "C" {
 #define DLED3			LATDbits.LATD3
 #define DLED4			LATDbits.LATD4
 #define DLED5			LATDbits.LATD5
-//#define DLED6			LATDbits.LATD4
-//#define DLED7			LATDbits.LATD5
-	
-#define DELAY_TOGGLE	LATAbits.LATA6
+	//#define DLED6			LATDbits.LATD4
+	//#define DLED7			LATDbits.LATD5
+
+#define DELAY_TOGGLE		LATAbits.LATA6
 
 #define BLED0			LATAbits.LATA2
 #define BLED1			LATAbits.LATA3
 
 #define SLED			LATEbits.LATE0
-#define RS				LATEbits.LATE1
-#define CSB				LATEbits.LATE2
-#define CSA				LATAbits.LATA7
-	
+#define RS			LATEbits.LATE1
+#define CSB			LATEbits.LATE2
+#define CSA			LATAbits.LATA7
+
 #define TEMP_DIODE		29	// internal chip temperature diode
 #define CTMU_CHAN		28	// mux disconnected for lowest possible measurement capacitor
 #define CTMU_ZERO		54
 #define CTMU_2M			74
 #define CTMU_RES_PS		500 // 500ps per ADC count
-#define CTMU_ADC_METER	10	// counts per meter
+#define CTMU_ADC_METER		10	// counts per meter
 
-#define EADOGM_CMD_CLR 1
+#define EADOGM_CMD_CLR		1
 #define EADOGM_CMD_CURSOR_ON     0b00001111
 #define EADOGM_CMD_CURSOR_OFF    0b00001100
 #define EADOGM_CMD_DISPLAY_ON    0b00001100
@@ -79,7 +79,7 @@ extern "C" {
 #define EADOGM_CMD_SELECT_R0     0b00011000
 #define EADOGM_CMD_SELECT_R1     0b00010000
 #define EADOGM_CMD_SET_TABLE2    0b00101010
-#define EADOGM_COLSPAN 16
+#define EADOGM_COLSPAN		16
 
 #ifdef	__cplusplus
 }
